b0cfa3ef0947be5f29071a7ffb73e7969d51074f
Sam Odras <odrassam@gmail.com>
2021-11-23 16:25:42 +0700

    Setup Router, Setup React Lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { lazy, Suspense } from 'react';
+import { Switch, Router, Route } from "react-router-dom";
+import history from "./history";
+import Spinner from "./components/spinner/spinner.component";
 
-function App() {
+const MainPage = lazy(() => import("./pages/main-page/main-page.component"))
+const DetailPage = lazy(() => import("./pages/detail-page/detail-page.component"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={history}>
+      <Suspense fallback={<Spinner/>}>
+        <Switch>
+          <Route path="/" exact component={MainPage}/>
+          <Route path="/detail" exact component={DetailPage}/>
+        </Switch>
+      </Suspense>
+    </Router>
+  )
 }
 
 export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
